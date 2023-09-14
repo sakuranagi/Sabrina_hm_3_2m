a1c117f02f88ccbeff73cb71d96dc1539c898d8c
sakuranagi <sabrinamuhammadimran@gmail.com>
2023-09-14 13:15:53 +0600

    Добавила пару строчек кода, для понимания сколько денег на счете

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,15 @@ public class Main {
         user.deposit(15000);
         while (true) {
             try {
+                System.out.println("На вашем счету: " + user.getAmount());
                 user.withDraw(6000);
                 System.out.println("Снято 6000.0. Остаток на счете " + user.getAmount());
             } catch (LimitException e) {
                 System.out.println(e.getMessage());
                 try {
                     user.withDraw((int) e.getRemainingAmount());
-                    System.out.println("Снято " + e.getRemainingAmount() + " сом. Остаток на счете: 0 сом.");
+                    System.out.println("На вашем счету осталось: " + e.getRemainingAmount());
+                    System.out.println("Снято " + e.getRemainingAmount() + " сом. Остаток на счете: " + user.getAmount() +" сом.");
                 } catch (LimitException ex) {
                     System.out.println(e.getMessage());
                 }
